4a3e4b6a6cafc28aa0b7dcd85cd81fe754fcb955
Demitrious Kelly <apokalyptik@apokalyptik.com>
2014-04-08 16:46:38 -0700

    add client.InsecureSkipVerify, client.Prefix, and client.Debug methods mainly useful in testing/development and/or for Automattic employees

--- wpcom.go
+++ wpcom.go
@@ -3,6 +3,7 @@
 package wpcom
 
 import (
+	"crypto/tls"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -14,21 +15,57 @@ import (
 const PREFIX = "https://public-api.wordpress.com/rest/v1/"
 
 type client struct {
-	token string
+	httpClient *http.Client
+	prefix     string
+	token      string
+	debug      bool
+}
+
+// Set the URL Prefix for the API client. This should normally not change unless you are
+// an Automattic developer with a WordPress.com development environment testing changes.
+// This option should *never* be overridden outside this specific circumstance
+func (c *client) Prefix(prefix string) {
+	c.prefix = prefix
+}
+
+// Enable or Disable Verification of the remote SSL Certificate. The client verifies by
+// default, however, for Automattic developers with test environments the cert hostname
+// does not match the request hostname. This function can be used to tell the client that
+// This is OK. This option should *never* be disabled outside this specific circumstance
+func (c *client) InsecureSkipVerify(want bool) {
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: want},
+	}
+	c.httpClient = &http.Client{Transport: tr}
+}
+
+// Turn debugging on or off.  When set to true request and response information will be
+// logged using log.Printf()
+func (c *client) Debug(debug bool) {
+	c.debug = debug
 }
 
 func (c *client) fetch(suffix string) (js []byte, err error) {
-	url := fmt.Sprintf("%s%s", PREFIX, suffix)
-	client := new(http.Client)
+	url := fmt.Sprintf("%s%s", c.prefix, suffix)
 	req, err := http.NewRequest("GET", url, nil)
+	req.Host = "public-api.wordpress.com"
 	if c.token != "" {
 		req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", c.token))
 	}
-	resp, err := client.Do(req)
+	if c.debug {
+		log.Printf("Request: %+v\n\nError: %+v", req, err)
+	}
+	resp, err := c.httpClient.Do(req)
+	if c.debug {
+		log.Printf("Response: %+v\n\nError: %+v", resp, err)
+	}
 	if err != nil {
 		return js, err
 	}
 	js, err = ioutil.ReadAll(resp.Body)
+	if c.debug {
+		log.Printf("Response Text: %+v\n\nError: %+v", string(js), err)
+	}
 	return js, err
 }
 
@@ -61,6 +98,8 @@ func (c *client) read(js []byte, into interface{}) error {
 // https://developer.wordpress.com/docs/oauth2/
 func New(access_token string) *client {
 	client := new(client)
+	client.prefix = PREFIX
+	client.InsecureSkipVerify(false)
 	client.token = access_token
 	return client
 }
