02202d75ce5ca2aadeae28eec6c1187b368e8a18
Demitrious Kelly <apokalyptik@apokalyptik.com>
2014-04-15 14:48:14 -0400

    godoc updates

--- me.go
+++ me.go
@@ -7,6 +7,15 @@ import (
 	"strconv"
 )
 
+// A structure representing the user (or lack thereof for anonymous API usage)
+// associated with the API calls.  Several API functions are attached to this
+// structure when it makes sense for those API calls in no other context than
+// that they are attached to an authenticated user. Notifications for example.
+// You should not create a Me struct directly but instead use the Me() method
+// on a Client struct. The reason for this is that a proper client for API
+// calls will be embedded in the Me struct only when initialized in this fashion.
+// The embedded client will retain the key debug, etc, settings from the creating
+// Client.
 type Me struct {
 	client       *Client
 	ID           int                    `json:"ID"`
@@ -23,6 +32,12 @@ type Me struct {
 	ErrorMessage string                 `json:"message"`
 }
 
+// Fetch, or re-fetch, details about the current API user.  The method updates
+// the existing struct with new data where applicable from the results of this
+// function call.  This is especially useful for Me structs initially created
+// without fetching the user info (which is possible by passing false to the Me
+// method on a Client struct)
+// see: https://developer.wordpress.com/docs/api/1/get/me/
 func (m *Me) Get() error {
 	js, err := m.client.fetch("me", O(), O())
 	if err != nil {
@@ -32,6 +47,10 @@ func (m *Me) Get() error {
 	return err
 }
 
+// Fetch the notifications for a user.  The opt argument allows you to
+// specify query parameters to attach to the API call.  See
+// https://developer.wordpress.com/docs/api/1/get/notifications/
+// for possible options, and information about the data it returns
 func (m *Me) Notifications(opt *Options) (NotificationsResponse, error) {
 	rval := NotificationsResponse{}
 	js, err := m.client.fetch("notifications/", opt, O())
@@ -43,6 +62,8 @@ func (m *Me) Notifications(opt *Options) (NotificationsResponse, error) {
 	return rval, err
 }
 
+// Fetch information about a specific notification via it's note ID.
+// See: https://developer.wordpress.com/docs/api/1/get/notifications/%24note_ID/
 func (m *Me) Notification(id int64) (Notification, error) {
 	rval := NotificationsResponse{}
 	js, err := m.client.fetch(fmt.Sprintf("notifications/%d", id), O(), O())
@@ -58,6 +79,8 @@ func (m *Me) Notification(id int64) (Notification, error) {
 	}
 }
 
+// Set the timestamp of the most recently seen notification for the current user
+// See: https://developer.wordpress.com/docs/api/1/post/notifications/seen/
 func (m *Me) NotificationsSeen(timestamp int64) (success bool, err error) {
 	js, err := m.client.fetch("notifications/seen", O(), O().Set("time", timestamp))
 	if err != nil {
@@ -84,6 +107,9 @@ func (m *Me) NotificationsSeen(timestamp int64) (success bool, err error) {
 	return
 }
 
+// Mark a set of notifications as read.  The l map being passed matches the
+// "counts" request parameter 1:1  from the Api documentation.  See:
+// https://developer.wordpress.com/docs/api/1/post/notifications/read/
 func (m *Me) NotificationsRead(l map[int64]int64) (updated map[int64]bool, err error) {
 	updated = make(map[int64]bool)
 	postOptions := new(Options)
